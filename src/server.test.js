49d024659756145666e211f1ed1f5a533f377925
Troy P Miller <73154493+TroubleTeeroy@users.noreply.github.com>
2021-05-08 01:46:08 -0400

    server.test.js complete

--- src/server.test.js
+++ src/server.test.js
@@ -1,7 +1,62 @@
+const http = require('http')
+
 const router = require('./server')
 
 describe('router', () => {
+    let req
+    let res
+
+    beforeEach(() => {
+        req = new http.IncomingMessage()
+        res = new http.ServerResponse(req)
+        res.setHeader = jest.fn()
+        res.writeHead - jest.fn()
+        res.end - jest.fn()        
+    })
+
     test('is a function', () => {
         expect(typeof router === 'function').toBe(true)
     })
+
+    describe('receives a request for an unsupported URL path', () => {
+        beforeEach(() => {
+            req.url = '/invalid-url-path'
+        })
+
+        test('sets Content-Type header', async () => {
+            await router (req, res)
+            expect(res.setHeader).toHaveBeenCalledWith('Content-type', 'application/json')
+        })
+
+        test('responds with 404 status code', async () => {
+            await router(req, res)
+            expect(res.writeHead).toHaveBeenCalledWith(404)
+        })
+
+        test('responds with a JSON string in the body', async () => {
+            await router(req, res)
+            expect(res.end).toHaveBeenCalledWith(JSON.stringify({error:'Resource not found'}))
+        })
+    })
+    
+        describe('receives a request for /ping', () => {
+            beforeEach(() => {
+            req.url = '/ping'
+            })
+        
+            test('sets Content-Type header', async () => {
+            await router(req, res)
+            expect(res.setHeader).toHaveBeenCalledWith('Content-Type', 'application/json')
+            })
+        
+            test('responds with 200 status code', async () => {
+            await router(req, res)
+            expect(res.writeHead).toHaveBeenCalledWith(200)
+            })
+        
+            test('responds with a JSON string in the body', async () => {
+            await router(req, res)
+            expect(res.end).toHaveBeenCalledWith(JSON.stringify({status: 'healthy'}))
+            })
+        })
 })
